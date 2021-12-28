35f904ae320bdae251fc079257bc67b368b0bc24
Sergio Luis Anaya Romero <sergio.anaya@multienlace.com.co>
2021-12-28 14:42:56 -0500

    motivos de llamda solicitud de servicio

--- src/test/java/co/konecta/sura/certificate/stepsdefinitions/parameterization/CallReasonsParameterizationStepsDefinitions.java
+++ src/test/java/co/konecta/sura/certificate/stepsdefinitions/parameterization/CallReasonsParameterizationStepsDefinitions.java
@@ -1,16 +1,21 @@
 package co.konecta.sura.certificate.stepsdefinitions.parameterization;
 
+import co.konecta.sura.certificate.task.parameterization.CallReasonsTask.ServiceRequestTask;
 import co.konecta.sura.certificate.userinterface.home.HomePage;
+import co.konecta.sura.certificate.userinterface.parameterization.CallReasonsParameterizationPage;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.ensure.Ensure;
 import net.thucydides.core.annotations.Managed;
 import org.openqa.selenium.WebDriver;
 
 import static co.konecta.sura.certificate.userinterface.HomePage.BUTTON_INITIAL_TAB;
+import static co.konecta.sura.certificate.userinterface.home.HomePage.MODAL_VALIDATION;
 import static co.konecta.sura.certificate.userinterface.home.HomePage.OPTION_MENU_PARAMETERIZATION;
 
 public class CallReasonsParameterizationStepsDefinitions {
@@ -39,6 +44,21 @@ public class CallReasonsParameterizationStepsDefinitions {
     }
     @And("^I select button create call reasons parameterization$")
     public void ISelectButtonCreateCallReasonsParameterization(){
-        actor.attemptsTo();
+        actor.attemptsTo(Click.on(CallReasonsParameterizationPage.BUTTON_CREATE_CALL_REASONS));
+    }
+    @And("^I write information in call reasons parameterization and (.*) and (.*)$")
+    public void IWriteInformationInCallReasonsParameterization(String name, String line){
+actor.attemptsTo(ServiceRequestTask.writeInformationServiceRequest(name,line));
+
+    }
+    @And("^I Select button save call reasons parameterization$")
+    public void ISelectButtonSaveCallReasonsParameterization(){
+        actor.attemptsTo(Click.on(CallReasonsParameterizationPage.BUTTON_SAVE_CALL_REASONS));
+    }
+    @Then("^I view the modal save call reasons parameterization (.*)$")
+    public void IViewTheModalSaveCallReasonsParameterization(String message) throws InterruptedException {
+        Thread.sleep(2000);
+        actor.attemptsTo(
+                Ensure.that(MODAL_VALIDATION).text().isEqualTo(message));
     }
 }
--- src/test/resources/features/callreasonsparameterization.feature
+++ src/test/resources/features/callreasonsparameterization.feature
@@ -10,8 +10,9 @@ Feature: Call reasons parameterization
     When I entering in the application with user <user> and password <password>
     And I enter call reasons parameterization
     And I select button create call reasons parameterization
-    And I write information in call reasons parameterization and <name> and <line> and save
-    Then I view the modal save  call reasons parameterization <modalmessage>
+    And I write information in call reasons parameterization and <name> and <line>
+    And I Select button save call reasons parameterization
+    Then I view the modal save call reasons parameterization <modalmessage>
 
     Examples:
       | user         | password       | name                   | line | modalmessage                      |
