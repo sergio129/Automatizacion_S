4adef068e512a14d777e9570ae94eed082d7e90a
Sergio Luis Anaya Romero <sergio.anaya@multienlace.com.co>
2022-02-03 16:18:33 -0500

    TestCreacionAsociacionListas

---
+++ src/test/java/co/konecta/sura/certificate/stepsdefinitions/IniciarNavegadorStepsDefinitions.java
@@ -0,0 +1,20 @@
+package co.konecta.sura.certificate.stepsdefinitions;
+
+import co.konecta.sura.certificate.Interfaces.Inicio.HomePage;
+import cucumber.api.java.Before;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
+
+public class IniciarNavegadorStepsDefinitions {
+    @Managed(driver = "chrome")
+    WebDriver driver;
+    private final Actor actor = Actor.named("Usuario");
+    private final HomePage homePage = new HomePage();
+
+    @Before
+    public void setUp() {
+        actor.can(BrowseTheWeb.with(driver));
+    }
+}
---
+++ src/test/java/co/konecta/sura/certificate/stepsdefinitions/listas/ListasAsociarListaStepsDefinitions.java
@@ -0,0 +1,8 @@
+package co.konecta.sura.certificate.stepsdefinitions.listas;
+
+import co.konecta.sura.certificate.stepsdefinitions.IniciarNavegadorStepsDefinitions;
+
+public class ListasAsociarListaStepsDefinitions extends IniciarNavegadorStepsDefinitions {
+
+
+}
