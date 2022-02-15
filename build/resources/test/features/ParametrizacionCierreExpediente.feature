84e79e58d1bebe5da22257c6e1a2380ca1c665c1
Sergio Luis Anaya Romero <sergio.anaya@grupokonecta.com>
2022-02-15 14:24:21 -0500

    TestCreacionParametrizacionCierreExpediente

--- build/resources/test/features/ParametrizacionCierreExpediente.feature
+++ build/resources/test/features/ParametrizacionCierreExpediente.feature
@@ -12,5 +12,18 @@ Feature: Creacion, Edicion y eliminacion de parametrizacion cierre de expediente
     And Seleccionamos el boton guardar Parametrizacion Cierre de Expediente
     Then Se visualiza mensaje de la modal<modalmensaje>
     Examples:
-      | usuario      | contrasena     | linea            | servicio         | Campo    | modalmensaje                    |
-      | Sergio.anaya | Colombia_2026* | Emergencia Salud | Emergencia Salud | Celosias | Registro guardado correctamente |
+      | usuario      | contrasena     | linea            | servicio         | Campo    | modalmensaje                           |
+      | Sergio.anaya | Colombia_2026* | Emergencia Salud | Emergencia Salud | Celosias | Parametrización agregada correctamente |
+
+  @TestEdicionParametrizacionCierreExpediente
+  Scenario Outline: Edicion Exitosa de Prefijos
+    When Ingresamos a la aplicacion con usuario<usuario> y contraseña<contrasena>
+    And Entramos Parametrizacion Cierre de Expediente
+    And Buscamos Parametrizacion Cierre de expediente por Linea:"Emergencia Salud", Servicio:"Emergencia Salud"
+    And Selecionamos el boton Editar Parametrizacion Cierre de Expediente
+    And Escribimos la informacion de parametrizacion Cierre de Expediente<linea>,<servicio>,<Campo>
+    And Seleccionamos el boton guardar Parametrizacion Cierre de Expediente
+    Then Se visualiza mensaje de la modal<modalmensaje>
+    Examples:
+      | usuario      | contrasena     | linea  | servicio     | Campo                | modalmensaje                           |
+      | Sergio.anaya | Colombia_2026* | Viajes | Certificados | ¿Concluido servicio? | Parametrización agregada correctamente |
--- src/test/java/co/konecta/sura/certificate/stepsdefinitions/Parametrizacion/CierreDeExpedientesStepsDefinitions.java
+++ src/test/java/co/konecta/sura/certificate/stepsdefinitions/Parametrizacion/CierreDeExpedientesStepsDefinitions.java
@@ -8,8 +8,11 @@ import cucumber.api.java.en.Given;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.Hit;
 import net.serenitybdd.screenplay.actions.MoveMouse;
 import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
 import static co.konecta.sura.certificate.Interfaces.HomePage.BUTTON_INITIAL_TAB;
@@ -27,7 +30,8 @@ public class CierreDeExpedientesStepsDefinitions {
     }
 
     @Given("Gestion la Creacion, Edicion y eliminacion de parametrizacion cierre de expediente")
-    public void GestionLaCreacionEdicionEliminacionDeParametrizacionCierreDeExpediente(){}
+    public void GestionLaCreacionEdicionEliminacionDeParametrizacionCierreDeExpediente() {
+    }
 
     @And("^Entramos Parametrizacion Cierre de Expediente$")
     public void entramosParametrizacionCierreDeExpediente() throws InterruptedException {
@@ -47,7 +51,40 @@ public class CierreDeExpedientesStepsDefinitions {
 
     @And("^Escribimos la informacion de parametrizacion Cierre de Expediente(.*),(.*),(.*)$")
     public void escribimosLaInformacionDeParametrizacionCierreDeExpedienteLineaServicioCampo(String linea, String servicio, String Campo) {
-        actor.has(CierreExpedientesTask.EscribirInformacionCierreExpediente(linea,servicio,Campo));
+        actor.has(CierreExpedientesTask.EscribirInformacionCierreExpediente(linea, servicio, Campo));
 
+    }
+
+    @And("^Seleccionamos el boton guardar Parametrizacion Cierre de Expediente$")
+    public void seleccionamosElBotonGuardarParametrizacionCierreDeExpediente() {
+        actor.has(Click.on(BOTON_GUARDAR_CIERRE_EXPEDIENTE));
+    }
+
+
+    @And("^Buscamos Parametrizacion Cierre de expediente por Linea:\"([^\"]*)\", Servicio:\"([^\"]*)\"$")
+    public void buscamosParametrizacionCierreDeExpedientePorLineaServicio(String linea, String servicio) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        Thread.sleep(3000);
+        actor.has(
+                Click.on(CAMPO_LINEA_INICIO),
+                Enter.theValue(linea).into(BUSCAR_LINEA_INICIO),
+                Click.on(SELECCIONAR_LINEA_INICIO),
+                Hit.the(Keys.ESCAPE).into(BUSCAR_LINEA_INICIO),
+                Click.on(CAMPO_SERVICIO_INICIO),
+                Enter.theValue(servicio).into(BUSCAR_SERVICIO_INICIO).thenHit(Keys.ENTER).thenHit(Keys.ESCAPE),
+                Click.on(BOTON_BUSCAR_CIERRE_EXPEDIENTE)
+        );
+        Thread.sleep(3000);
+
+    }
+
+    @And("^Selecionamos el boton Editar Parametrizacion Cierre de Expediente$")
+    public void selecionamosElBotonEditarParametrizacionCierreDeExpediente() {
+        actor.has(Click.on(BOTON_EDITAR_CIERRE_EXPEDIENTE));
+    }
+
+    @And("^Seleccionamos el Boton Eliminar Parametrizacion Cierre de Expediente$")
+    public void seleccionamosElBotonEliminarParametrizacionCierreDeExpediente() {
+        actor.has(Click.on(BOTON_ELIMINAR_CIERRE_EXPEDIENTE), Click.on(OPCION_SI_ELIMINAR_CIERRE_EXPEDIENTE));
     }
 }
