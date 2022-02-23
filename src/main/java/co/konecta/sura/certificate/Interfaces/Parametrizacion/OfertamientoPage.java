f04e2b8a1027a772c5d69f77be13e52d50fbe1ce
Sergio Luis Anaya Romero <sergio.anaya@grupokonecta.com>
2022-02-23 13:54:14 -0500

    Bot de ofertamiento

--- src/main/java/co/konecta/sura/certificate/Interfaces/Parametrizacion/OfertamientoPage.java
+++ src/main/java/co/konecta/sura/certificate/Interfaces/Parametrizacion/OfertamientoPage.java
@@ -9,10 +9,24 @@ public class OfertamientoPage {
     public static final Target CAMPO_NOMBRE_DE_OFERTAMIENTO = Target.the("CAMPO NOMBRE DE PARAMETRIZACION DE OFERTAMIENTO").located(By.id("createInputNameOfferChanttigoParametrization"));
     public static final Target CAMPO_COORDINACION_DE_OFERTAMIENTO = Target.the("CAMPO COORDINACION DE PARAMETRIZACION DE OFERTAMIENTO").located(By.id("selectCoordinationManagerOfferChanttigoParametrization"));
     public static final Target SELECCIONAR_COORDINACION_DE_OFERTAMIENTO = Target.the("SELECCIONAR OPCION DE COORDINACION DE PARAMETRIZACION DE OFERTAMIENTO").locatedBy("//*[@id='listCoordinationManagerOfferChanttigoParametrization']/span[contains(text(),'{0}')]");
-    public static final Target CAMPO_TIPO_OFERTAMIENTO = Target.the("CAMPO TIPO OFERTAMIENTO PARAMETRIZACION DE OFERTAMIENTO").located(By.id("selectOfferTypeOfferChanttigoParametrization"));
-
-
-
+    public static final Target CAMPO_TIPO_OFERTAMIENTO = Target.the("CAMPO TIPO PARAMETRIZACION DE OFERTAMIENTO").located(By.id("selectOfferTypeOfferChanttigoParametrization"));
+    public static final Target BUSCAR_TIPO_OFERTAMIENTO = Target.the("BUSCAR OFERTAMIENTO PARAMETRIZACION DE OFERTAMIENTO").located(By.cssSelector("[placeholder='Buscar']"));
+    public static final Target CAMPO_TIEMPO_OFERTAMIENTO = Target.the("CAMPO TIEMPO DE PARAMETRIZACION DE OFERTAMIENTO").located(By.id("createInputTimeOfferChanttigoParametrization"));
+    public static final Target CAMPO_LINEA_OFERTAMIENTO = Target.the("CAMPO LINEA DE PARAMETRIZACION DE OFERTAMIENTO").located(By.id("selectLineOfferChanttigoParametrization"));
+    public static final Target BUSCAR_LINEA_OFERTAMIENTO = Target.the("BUSCAR LINEA DE PARAMETRIZACION DE OFERTAMIENTO").located(By.id("inputCreateLineOfferChanttigoParametrization"));
+    public static final Target SELECCIONAR_LINEA_OFERTAMIENTO = Target.the("SELECCIONAMOS LA LINEA DE PARAMETRIZACION DE OFERTAMIENTO").locatedBy("//mat-option/span[contains(text(),' {0} ')]");
+    public static final Target CAMPO_SERVICIO_OFERTAMIENTO = Target.the("CAMPO SERVICIO PARAMETRIZACION DE OFERTAMIENTO").located(By.id("selectServiceOfferChanttigoParametrization"));
+    public static final Target BUSCAR_SERVICIO_OFERTAMIENTO = Target.the("BUSCAR SERVICIO DE PARAMETRIZACION DE OFERTAMIENTO").located(By.id("inputCreateServiceOfferChanttigoParametrization"));
+    public static final Target CAMPO_DEPARTAMENTO_OFERTAMIENTO = Target.the("CAMPO DEPARTAMENTO PARAMETRIZACION DE OFERTAMIENTO").located(By.id("selectDepartamentOfferChanttigoParametrization"));
+    public static final Target BUSCAR_DEPARTAMENTO_OFERTAMIENTO = Target.the("BUSCAR DEPARTAMENTO DE PARAMETRIZACION DE OFERTAMIENTO").located(By.id("inputCreateDepartamentOfferChanttigoParametrization"));
+    public static final Target SELECCIONAR_DEPARTAMENTO_OFERTAMIENTO = Target.the("SELECCIONAMOS EL DEPARTAMENTO DE PARAMETRIZACION DE OFERTAMIENTO").locatedBy("//mat-option/span[contains(text(),' {0} ')]");
+    public static final Target CAMPO_MUNICIPIO_OFERTAMIENTO = Target.the("CAMPO DEPARTAMENTO PARAMETRIZACION DE OFERTAMIENTO").located(By.id("selectMunicipalitiesOfferChanttigoParametrization"));
+    public static final Target BUSCAR_MUNICIPIO_OFERTAMIENTO = Target.the("BUSCAR DEPARTAMENTO DE PARAMETRIZACION DE OFERTAMIENTO").located(By.id("inputCreateMunicipalitiesOfferChanttigoParametrization"));
+    public static final Target BOTON_GUARDAR_OFERTAMIENTO = Target.the("BOTON PARA GUARDAR DE PARAMETRIZACION DE OFERTAMIENTO").located(By.id("btnSaveOfferChanttigoParametrization"));
+    public static final Target BOTON_VISUALIZAR_OFERTAMIENTO = Target.the("BOTON PARA VISUALIZAR DE PARAMETRIZACION DE OFERTAMIENTO").located(By.id("btnVisibilityOfferChanttigoParametrization"));
+    public static final Target BOTON_EDITAR_OFERTAMIENTO = Target.the("BOTON PARA EDITAR DE PARAMETRIZACION DE OFERTAMIENTO").located(By.id("btnEditOfferChanttigoParametrization"));
+    public static final Target CAMPO_BUSCAR_OFERTAMIENTO = Target.the("BUSCAR DE PARAMETRIZACION DE OFERTAMIENTO").located(By.id("inputNameOfferChanttigoParametrization"));
+    public static final Target BOTON_ACTIVAR_OFERTAMIENTO = Target.the("BOTON ACTIVAR/DESACTIVAR PARAMETRIZACION DE OFERTAMIENTO").located(By.id("btnStatusOfferChanttigoParametrization0"));
 
 
 }
--- src/test/resources/features/ParametrizacionOfertamiento.feature
+++ src/test/resources/features/ParametrizacionOfertamiento.feature
@@ -1,14 +1,41 @@
 Feature: Parametrizacion de Bot de ofertamiento
 
   Background:
-    Given :Creacion, Edicion y Activar/Desactivar Parametrizacion
+    Given Creacion, Edicion y Activar/Desactivar Parametrizacion
 
+  @TestCreacionOfertamiento
   Scenario Outline:Creacion Exitosa de parametrizacion Ofertamiento
     When Ingresamos a la aplicacion con usuario<usuario> y contraseña<contrasena>
     And Entramos a Parametrizacion Ofertamiento
     And Selecionamos el boton crear parametrizacion de Ofertamiento
-    And Digitamos la informacion de la parametrizacion de ofertamiento<Nombre>,<Coordinacion>,<Ofertamiento>,<Tiempo>,<Line>,<Servicio>,<Departamento>,<Municipio>
+    And Digitamos la informacion de la parametrizacion de ofertamiento<Nombre>,<Coordinacion>,<Ofertamiento>,<Tiempo>,<Linea>,<Servicio>,<Departamento>,<Municipio>
+    And Selecionamos el Boton de guardar Parametrizacion de Ofertamiento
     Then Se visualiza mensaje de la modal<modalmensaje>
     Examples:
-      | usuario      | contrasena     | Nombre                 | Coordinacion | Ofertamiento      | Tiempo | Line             | Servicio         | Departamento | Municipio | modalmensaje                      |
-      | Sergio.anaya | Colombia_2026* | Pruebas_automatizacion | Si           | Abogados Rionegro | 15     | Emergencia salud | Emergencia salud | Cordoba      | Lorica    | Registros guardados correctamente |
+      | usuario      | contrasena     | Nombre                | Coordinacion | Ofertamiento      | Tiempo | Linea            | Servicio         | Departamento | Municipio | modalmensaje                    |
+      | Sergio.anaya | Colombia_2026* | PruebasAutomatizacion | No           | Abogados Rionegro | 15     | Emergencia salud | Emergencia salud | Cordoba      | Lorica    | Registro guardado correctamente |
+
+  @TestEdicionOfertamiento
+  Scenario Outline:Edicion Exitosa de parametrizacion Ofertamiento
+    When Ingresamos a la aplicacion con usuario<usuario> y contraseña<contrasena>
+    And Entramos a Parametrizacion Ofertamiento
+    And Buscamos Parametrizacion de Ofertamiento por nombre: "PruebasAutomatizacion"
+    And Selecionamos el boton Editar parametrizacion de Ofertamiento
+    And Digitamos la informacion de la parametrizacion de ofertamiento<Nombre>,<Coordinacion>,<Ofertamiento>,<Tiempo>,<Linea>,<Servicio>,<Departamento>,<Municipio>
+    And Selecionamos el Boton de guardar Parametrizacion de Ofertamiento
+    Then Se visualiza mensaje de la modal<modalmensaje>
+    Examples:
+      | usuario      | contrasena     | Nombre                | Coordinacion | Ofertamiento      | Tiempo | Linea | Servicio         | Departamento | Municipio | modalmensaje                    |
+      | Sergio.anaya | Colombia_2026* | PruebasAutomatizacion | No           | Abogados Rionegro | 15     | ARL   | Emergencia salud | Antioquia    | Medellin  | Registro guardado correctamente |
+
+
+  @TestActivarDesactivarOfertamiento
+  Scenario Outline:Activar/desactivar parametrizacion Ofertamiento
+    When Ingresamos a la aplicacion con usuario<usuario> y contraseña<contrasena>
+    And Entramos a Parametrizacion Ofertamiento
+    And Buscamos Parametrizacion de Ofertamiento por nombre: "PruebasAutomatizacion"
+    And Selecionamos el boton de Activar/Desactivar Parametrizacion Ofertamiento
+    Then Se visualiza mensaje de la modal<modalmensaje>
+    Examples:
+      | usuario      | contrasena     | modalmensaje                                       |
+      | Sergio.anaya | Colombia_2026* | Parametrización habilitada/deshabilitada con éxito |
