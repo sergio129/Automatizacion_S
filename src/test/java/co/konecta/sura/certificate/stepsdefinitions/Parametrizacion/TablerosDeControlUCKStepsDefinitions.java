ec4a64a5eaf57afd0330325d7fee70dc26e61462
Sergio Luis Anaya Romero <sergio.anaya@grupokonecta.com>
2022-02-25 12:08:22 -0500

    TestEdicionUCK

--- src/test/java/co/konecta/sura/certificate/stepsdefinitions/Parametrizacion/TablerosDeControlUCKStepsDefinitions.java
+++ src/test/java/co/konecta/sura/certificate/stepsdefinitions/Parametrizacion/TablerosDeControlUCKStepsDefinitions.java
@@ -1,6 +1,7 @@
 package co.konecta.sura.certificate.stepsdefinitions.Parametrizacion;
 
 import co.konecta.sura.certificate.Interfaces.Inicio.HomePage;
+import co.konecta.sura.certificate.Tareas.Parametrizacion.TablerosDeControlUCKTask;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
@@ -41,8 +42,14 @@ public class TablerosDeControlUCKStepsDefinitions {
     public void hacemosUnaBusquedaPorFechaDeTablerosUck(String fecha) throws InterruptedException {
         actor.has(
                 Click.on(FECHA_TABLERO_CONTROL_UCK),
-                Click.on(SELECCIONAR_FECHA_UCK.of(String.valueOf(fecha)))
+                Click.on(SELECCIONAR_FECHA_UCK.of(String.valueOf(fecha))),
+                Click.on(BOTON_BUSCAR_UCK)
         );
 Thread.sleep(1000);
+    }
+
+    @And("^Escribimos la informacion de tableros uck(.*),(.*),(.*),(.*)$")
+    public void escribimosLaInformacionDeTablerosUckMotivoAseguradoHabilitarObservacion(String motivo,String asegurado,String habilitar,String observacion) {
+        actor.has(TablerosDeControlUCKTask.EscribirInformacionUCK(motivo,asegurado, habilitar, observacion));
     }
 }
