78f3803f6f03d75ddea4b7281aeff96d47aa3697
sergio129 <sanayaromero62@gmail.com>
2022-10-05 15:53:39 -0500

    Add files via upload

---
+++ src/test/java/co/konecta/sura/certificate/runners/PrincipalRunner.java
@@ -0,0 +1,18 @@
+package co.konecta.sura.certificate.runners;
+
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        glue = "co.konecta.sura.certificate.stepsdefinitions",
+        features = "src/test/resources/features/",
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        tags = "@TestCreacionCaso",
+        monochrome = true
+)
+
+public class PrincipalRunner {
+}
