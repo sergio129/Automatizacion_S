3908ca7d1f98aebeb01d189018ca5721ae20a90b
sergio129 <sanayaromero62@gmail.com>
2022-10-05 15:30:50 -0500

    Update LoginRunner.java

--- src/test/java/co/konecta/sura/certificate/runners/LoginRunner.java
+++ src/test/java/co/konecta/sura/certificate/runners/LoginRunner.java
@@ -5,15 +5,14 @@ import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
-    @RunWith(CucumberWithSerenity.class)
-    @CucumberOptions(
-            glue = "co.konecta.sura.certificate.stepsdefinitions",
-            features = "src/test/resources/features/login.feature",
-            snippets = CucumberOptions.SnippetType.CAMELCASE,
-            monochrome = true
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        glue = "co.konecta.sura.certificate.stepsdefinitions",
+        features = "src/test/resources/features/",
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        tags = "@TestCreacionCaso",
+        monochrome = true
+)
 
-    )
-
-    public class LoginRunner {
-
+public class PrincipalRunner {
 }
