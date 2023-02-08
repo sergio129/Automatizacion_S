0842e7947b4e853eb1e651eb4bdd1bae1aff8574
sergio129 <sanayaromero62@gmail.com>
2023-02-08 12:00:53 -0500

    Update PrincipalRunner.java

--- src/test/java/co/konecta/sura/certificate/runners/PrincipalRunner.java
+++ src/test/java/co/konecta/sura/certificate/runners/PrincipalRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         glue = "co.konecta.sura.certificate.stepsdefinitions",
         features = "src/test/resources/features/",
         snippets = CucumberOptions.SnippetType.CAMELCASE,
-        tags = "@TestPruebasLogin",
+        tags = "@TestPruebasLogin or  @TestCreacionCaso or  @TestCreacionCodigoSalida or @TestCaseComplete or  @TestCreacionPrefijos",
         monochrome = true
 )
 
